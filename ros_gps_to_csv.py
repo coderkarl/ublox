dffd2d333470e310dcc745604be7639e82044d11
Karl Kirsch <21990134+coderkarl@users.noreply.github.com>
2020-12-13 15:12:44 -0600

    Initial ros bag to csv

---
+++ ros_gps_to_csv.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python
+
+#http://docs.ros.org/api/rosbag_storage/html/c++/
+import rosbag
+from binascii import hexlify
+import rospy
+
+## LOAD BAG DATA
+#bag = rosbag.Bag('/home/karl/wheele_bags/wheele_add_compass_GPS_freeze_02Feb2018/2018-02-02-17-54-48.bag')
+#bag = rosbag.Bag('/home/karl/wheele_bags/wheele_more_speed_03Feb2018/2018-02-03-17-42-00.bag')
+
+## Get bag file(s) from current directory where this is run
+#import glob
+#files = glob.glob("*.bag")
+import fnmatch
+import os
+files = []
+for root, dirnames, filenames in os.walk('.'):
+    for filename in fnmatch.filter(filenames, '*.bag'):
+        files.append(os.path.join(root, filename))
+
+for f in files:
+    print f
+    bag = rosbag.Bag(f)
+    
+    print('fix')
+    csv_fix_name = f[0:-4]+'_fix.csv'
+    csv_fix = open(csv_fix_name,'w')
+    csv_fix.write('bagTime,frameTime,status,lat,lon,covar0,covar4\n')
+  
+    for k, (topic, msg, t) in enumerate(bag.read_messages('/ublox/fix')):
+        if(k==0):
+            t0 = t
+            
+        csv_fix.write('%.9f,%.9f,%d,%.9f,%.9f,%.3f,%.3f\n'
+          % ((t-t0).to_sec(), (msg.header.stamp-t0).to_sec(), msg.status.status,
+              msg.latitude, msg.longitude, msg.position_covariance[0], msg.position_covariance[4] ))
+    csv_fix.close()
+    
+    #print('gps_pose')
+    #csv_gps_pose_name = f[0:-4]+'_gps_pose.csv'
+    #csv_gps_pose = open(csv_gps_pose_name,'w')
+    #csv_gps_pose.write('bagTime,frameTime,x,y\n')
+  
+    #for k, (topic, msg, t) in enumerate(bag.read_messages('/gps_pose')):            
+    #    csv_gps_pose.write('%.9f,%.9f,%.3f,%.3f\n'
+    #      % ((t-t0).to_sec(), (msg.header.stamp-t0).to_sec(), msg.vector.x, msg.vector.y) )
+    #csv_gps_pose.close()
+    
+    print('gps_vel')
+    csv_gps_vel_name = f[0:-4]+'_gps_vel.csv'
+    csv_gps_vel = open(csv_gps_vel_name,'w')
+    csv_gps_vel.write('bagTime,frameTime,vx,vy\n')
+  
+    for k, (topic, msg, t) in enumerate(bag.read_messages('/ublox/fix_velocity')):            
+        csv_gps_vel.write('%.9f,%.9f,%.3f,%.3f\n'
+          % ((t-t0).to_sec(), (msg.header.stamp-t0).to_sec(), msg.twist.twist.linear.x, msg.twist.twist.linear.y) )
+    csv_gps_vel.close()
+    
+    print('gps_relpose')
+    csv_gps_vel_name = f[0:-4]+'_gps_relpose.csv'
+    csv_gps_vel = open(csv_gps_vel_name,'w')
+    csv_gps_vel.write('bagTime,relE,relN\n')
+  
+    for k, (topic, msg, t) in enumerate(bag.read_messages('/ublox/navrelposned')):            
+        csv_gps_vel.write('%.9f,%.3f,%.3f\n'
+          % ((t-t0).to_sec(), msg.relPosE/100.0, msg.relPosN/100.0) )
+    csv_gps_vel.close()
+    
+    bag.close()
